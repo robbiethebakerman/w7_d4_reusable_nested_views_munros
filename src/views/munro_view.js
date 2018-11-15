18db8f1803a26973079716748e4167168c045686
Robbie Baker <robbiethebakerman@googlemail.com>
2018-11-15 14:22:29 +0000

    Completes MVP

--- src/views/munro_view.js
+++ src/views/munro_view.js
@@ -6,9 +6,33 @@ const MunroView = function (munro, container) {
 };
 
 MunroView.prototype.render = function () {
-  const name = document.createElement('h2');
-  name.textContent = this.munro.name;
-  this.container.appendChild(name);
+  const divWrapper = document.createElement('div');
+  divWrapper.classList.add('munro-view');
+
+  const name = this.createTextElement('h2', this.munro.name, 'munro-name');
+  divWrapper.appendChild(name);
+
+  const infoList = this.createInfoList();
+  divWrapper.appendChild(infoList);
+
+  this.container.appendChild(divWrapper);
+};
+
+MunroView.prototype.createTextElement = function (type, textContent, cssClass) {
+  const element = document.createElement(type);
+  element.textContent = textContent;
+  element.classList.add(cssClass);
+  return element;
+};
+
+MunroView.prototype.createInfoList = function () {
+  const list = document.createElement('ul');
+  list.classList.add('munro-info-list');
+  const height = this.createTextElement('li', `Height: ${this.munro.height}`, 'munro-info-list-height');
+  const meaning = this.createTextElement('li', `Meaning of "${this.munro.name}": ${this.munro.meaning}`, 'munro-info-list-meaning');
+  list.appendChild(height);
+  list.appendChild(meaning);
+  return list;
 };
 
 module.exports = MunroView;
